7e14db94ed6e2612dbd6f6dd15291cc3b683235e
Ashutosh Sahu <47266286+ashu21sahu@users.noreply.github.com>
2019-04-26 12:57:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,6 +6,10 @@ $sql="select uid,password from login";
 $res=mysqli_query($link,$sql);
 while($row=mysqli_fetch_array($res))
 {
-	$uid=
+	if($uid==$row['uid'] && $pwd==$row['password'])
+	{
+		header("location:vote.php");
+		exit;
+	}	
 }
 ?>
